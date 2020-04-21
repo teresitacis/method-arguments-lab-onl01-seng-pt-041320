1c343fd1d08b9dab5e964a4a73588d567e96b541
teresita cisneros estrada <cisneros0831@gmail.com>
2020-04-21 05:25:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,4 +5,4 @@ def introduction_with_language(name,language)
  puts "Hi, my name is #{name} and I am learning to program in #{language}." 
 end  
 
-introduction_with_language ( Michael,"Typescript")
+introduction_with_language ( "Michael","Typescript")
