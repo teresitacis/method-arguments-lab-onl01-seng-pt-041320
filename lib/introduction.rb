0a5d031d0d370b63ee1a0695a18bd907048991ef
teresita cisneros estrada <cisneros0831@gmail.com>
2020-04-21 05:18:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,5 +2,5 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 def introduction_with_language 
- puts "Hi, my name is #{name}" 
+ puts "Hi, my name is #{name} and I am learning to code" 
 end  
